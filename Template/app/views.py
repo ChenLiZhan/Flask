fece00fbbf24bbdd2e5c82b241b5859f443d159c
chenlizhan <chung1350@hotmail.com>
2014-07-26 05:22:40 +0800

    Finish Template practice
    *Control statements
    *Loops
    *Template inheritance

--- Template/app/views.py
+++ Template/app/views.py
@@ -4,8 +4,23 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	user = { 'nickname'		: 'Lee'}
+	user = {'nickname'		: 'Lee'}
+	posts = [
+		{
+			'author': {
+				'nickname'	: 'John'
+			},
+			'body'	: 'Beautiful day in US'
+		},
+		{
+			'author': {
+				'nickname'	: 'Susan'
+			},
+			'body'	: 'The Aengers movie was so cool!'
+		}
+	]
 	return render_template('index.html',
 			title = 'Home',
-			user = user
+			user = user,
+			posts = posts
 		)
